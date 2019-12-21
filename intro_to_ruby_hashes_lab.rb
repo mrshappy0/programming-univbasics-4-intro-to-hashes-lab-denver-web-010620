2f300fab88cf80c6ee7fd2cf9ad969881e08ddba
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 16:57:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  
-  read_from_hash = hash[:key]
+  p hash[:key]
 end
 
 def update_counting_hash(hash, key)
